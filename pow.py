09af8fa261a6328b9d344eaee359a8fb27d39eb7
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2020-08-07 09:29:45 +0800

    Update pow.py

--- pow.py
+++ pow.py
@@ -1,69 +1,116 @@
-# coding:utf-8
-import time
+# packing function select a list txs = [...]
+# until > 1MB or no tx
+# verify if tx are valid
+# return txs to be used in add_block(transactions)
+# wait copy the verify function from other .py
+# add info in start in cli.py : # log.info("mined a block")
+
+"""
+1. search in txpool : delete invalid transactions
+2. sort the txpool
+3. start add transactions into the list : until > 1MB or no tx
+4. delete the packed transactions from the txpool
+4. return the list of transactions (to be used in add_block)
+"""
+
 import sys
-import utils
-from errors import NonceNotFoundError
-import pdb  # 7.11
+import threading
+import logging
+import time
+from txpool import *
+from block_chain import *
+from sorting import *
+from transactions import *
 from stopmine import StopMine
 
-class ProofOfWork(object):
-    """
-    pow
-    """
-    _N_BITS = 1    # e1
-    MAX_BITS = 256
-    MAX_SIZE = sys.maxsize
-    def __init__(self, block, n_bits=_N_BITS):
-        self._n_bits = n_bits
-        self._target_bits = 1 << (self.MAX_BITS - n_bits)
-        self._block = block
 
-    def _prepare_data(self, nonce):
-        data_lst = [str(self._block.block_header.prev_block_hash),
-                    str(self._block.block_header.hash_merkle_root),
-                    str(self._block.block_header.timestamp),
-                    str(self._block.block_header.height),
-                    str(nonce)]
-        return utils.encode(''.join(data_lst))
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger(__name__)
 
-    def run(self):
-        nonce = 0
-        found = False
-        hash_hex = None
-        # print('Mining a new block')   # change delete
-        while nonce < self.MAX_SIZE:
+
+class NotError(Exception):
+    pass
+
+
+def packing():
+    log.info("------into packing------")
+    bc1 = BlockChain()  # used to verify the transactions
+    tx_pool1 = TxPool()
+    total_fee = 0   # total fee 6.19
+    for tx1 in tx_pool1.txs:    # 1. search in txpool : delete invalid transactions
+        # if not bc1.verify_transaction(tx1):   # e1
+            # raise NotError('valid')
+            # tx_pool1.txs.remove(tx1)  # e1
+            # raise NotError('invalid')
+        if tx1.amount <= 0.1:   # e1
+            tx_pool1.txs.remove(tx1)    # e1
+        if type(tx1) == 'dict':
+            tx_pool1[tx_pool1.txs.index(tx1)] = Transaction.deserialize(tx1)    # change 6.22
+    # txs = sorting(tx_pool1.txs)    # 2. sort the txpool
+    # log.info("------after sorting------")
+    # log.info(txs)
+    selected_txs = []
+    if not tx_pool1.txs:
+        return [], 0
+    selected_tx = tx_pool1.txs[0]
+    log.info("------first for------")
+    for tx1 in tx_pool1.txs: # 3. start add transactions into the list : until > 1MB or no tx
+        if tx1.fee_size_ratio > selected_tx.fee_size_ratio and tx1.amount > 0.1:
+            selected_tx = tx1
+        # selected_txs.append(tx1)
+        # print(selected_txs[0].txid)
+        # if sys.getsizeof(selected_txs) > 1048576:
+            # raise NotError('bigger than 1 MB')
+            # selected_txs.remove(tx1)
+            # return selected_txs, total_fee  # change 6.20
+        # else:
+        #     remain_txs = []
+        #     for i in tx_pool1.txs:
+        #         if i.txid != tx1.txid:
+        #             remain_txs.append(i)
+        #     tx_pool1.txs = remain_txs    # 4. delete the packed transactions from the txpool
+        #     # tx_pool1.txs.remove(tx1)   # there are some problems with his remove function --> not use it
+        #     total_fee += tx1.amount    # add the fee
+        # if selected_txs:
+        #     log.info("------before return------")   # e1
+    selected_txs.append(selected_tx)
+    total_fee += selected_tx.amount
+    remain_txs = []
+    for i in tx_pool1.txs:
+        if i.txid != selected_tx.txid and i.amount > 0.1:
+            remain_txs.append(i)
+    tx_pool1.txs = remain_txs
+            # return selected_txs, total_fee  # e1
+    log.info("------before return------")
+    return selected_txs, total_fee  # change 6.20
+
+
+def finding_new_block():
+    i = 1
+    while i < 12:
+        try:
+            st = StopMine()
+            st.mine_h = i
+        except:
+            pass
+        bc1 = BlockChain()
+        tx3, total_fee = packing()
+        log.info("------return these information:" + str(tx3) + str(total_fee) + "------")
+        try:
+            if tx3:
+                bc1.add_block(tx3, total_fee)    # wait try when there's no transaction
+        except:
+            log.info("------fall behind in mine------")
             try:
                 st = StopMine()
-                if st.h >= st.mine_h:
-                    raise NonceNotFoundError
+                log.info("------with longest " + str(st.h) + " and local " + str(i) + "------")
+                while i < st.h:
+                    tx3, total_fee = packing()
+                    i += 1
             except:
                 pass
-            data = self._prepare_data(nonce)
-            hash_hex = utils.sum256_hex(data)
+        i += 1
 
-            hash_val = int(hash_hex, 16)
-            # sys.stdout.write("data: %s\n" % data) # 7.11
-            # sys.stdout.write("try nonce == %d\thash_hex == %s \n" % (nonce, hash_hex))    # 7.11
-            if (hash_val < self._target_bits):
-                found = True
-                break
-
-            nonce += 1
-        if found:
-            time.sleep(60)
-            pass
-        else:
-            # print('Not Found nonce')
-            raise NonceNotFoundError('nonce not found')
-        return nonce, hash_hex
-
-    def validate(self):
-        """
-        validate the block
-        """
-        data = self._prepare_data(self._block.block_header.nonce)
-        # print("data:"+str(data))    # change delete
-        hash_hex = utils.sum256_hex(data)
-        hash_val = int(hash_hex, 16)
-        # print(hash_hex) # change delete
-        return hash_val < self._target_bits
+# def start_find():
+#     t1 = threading.Thread(target=finding_new_block,args=()) # change
+#     t1.start()  # change
