d46e64f9d776b4fda36722f381e7aa502446ef2f
Shulin Ke <59651379+ksl20200108@users.noreply.github.com>
2020-08-12 11:35:09 +0800

    Add files via upload

--- block_chain.py
+++ block_chain.py
@@ -87,7 +87,7 @@ class BlockChain(object):
         block.deserialize(block_data)
         return block
 
-    def add_block(self, transactions, fee=0):
+    def add_block(self, transactions=[], fee=0):
         last_block = None
         while not last_block:
             last_block = self.get_last_block()
@@ -101,7 +101,10 @@ class BlockChain(object):
         keys = list(wallets.keys())
         w = wallets[keys[0]]
         coin_base_tx = self.coin_base_tx(w.address, fee)
-        transactions.insert(0, coin_base_tx)
+        if transactions:
+            transactions.insert(0, coin_base_tx)
+        else:
+            transactions = [coin_base_tx]
 
         utxo_set = UTXOSet()
         txs = utxo_set.clear_transactions(transactions)
--- packing.py
+++ packing.py
@@ -1,18 +1,3 @@
-# packing function select a list txs = [...]
-# until > 1MB or no tx
-# verify if tx are valid
-# return txs to be used in add_block(transactions)
-# wait copy the verify function from other .py
-# add info in start in cli.py : # log.info("mined a block")
-
-"""
-1. search in txpool : delete invalid transactions
-2. sort the txpool
-3. start add transactions into the list : until > 1MB or no tx
-4. delete the packed transactions from the txpool
-4. return the list of transactions (to be used in add_block)
-"""
-
 import sys
 import threading
 import logging
@@ -23,8 +8,7 @@ from sorting import *
 from transactions import *
 from stopmine import StopMine
 
-
-logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
 
@@ -34,57 +18,40 @@ class NotError(Exception):
 
 def packing():
     log.info("------into packing------")
-    bc1 = BlockChain()  # used to verify the transactions
+    bc1 = BlockChain()
     tx_pool1 = TxPool()
-    total_fee = 0   # total fee 6.19
-    for tx1 in tx_pool1.txs:    # 1. search in txpool : delete invalid transactions
-        # if not bc1.verify_transaction(tx1):   # e1
-            # raise NotError('valid')
-            # tx_pool1.txs.remove(tx1)  # e1
-            # raise NotError('invalid')
-        if tx1.amount <= 0.1:   # e1
-            tx_pool1.txs.remove(tx1)    # e1
+    if len(tx_pool1.txs) == 0:
+        log.info("oh no")
+        return [], "no"
+    total_fee = 0
+    for tx1 in tx_pool1.txs:
+        if tx1.amount <= 0.1:
+            tx_pool1.txs.remove(tx1)
         if type(tx1) == 'dict':
-            tx_pool1[tx_pool1.txs.index(tx1)] = Transaction.deserialize(tx1)    # change 6.22
-    # txs = sorting(tx_pool1.txs)    # 2. sort the txpool
-    # log.info("------after sorting------")
-    # log.info(txs)
+            tx_pool1[tx_pool1.txs.index(tx1)] = Transaction.deserialize(tx1)
     selected_txs = []
-    if not tx_pool1.txs:
-        return [], 0
-    selected_tx = tx_pool1.txs[0]
+    selected_tx = None
     log.info("------first for------")
-    for tx1 in tx_pool1.txs: # 3. start add transactions into the list : until > 1MB or no tx
-        if tx1.fee_size_ratio > selected_tx.fee_size_ratio and tx1.amount > 0.1:
+    for tx1 in tx_pool1.txs:
+        if selected_tx:
+            if tx1.fee_size_ratio > selected_tx.fee_size_ratio and tx1.amount > 0.1:
+                if tx1.verify():
+                    selected_tx = tx1
+        elif tx1.amount > 0.1 and tx1.verify():
             selected_tx = tx1
-        # selected_txs.append(tx1)
-        # print(selected_txs[0].txid)
-        # if sys.getsizeof(selected_txs) > 1048576:
-            # raise NotError('bigger than 1 MB')
-            # selected_txs.remove(tx1)
-            # return selected_txs, total_fee  # change 6.20
-        # else:
-        #     remain_txs = []
-        #     for i in tx_pool1.txs:
-        #         if i.txid != tx1.txid:
-        #             remain_txs.append(i)
-        #     tx_pool1.txs = remain_txs    # 4. delete the packed transactions from the txpool
-        #     # tx_pool1.txs.remove(tx1)   # there are some problems with his remove function --> not use it
-        #     total_fee += tx1.amount    # add the fee
-        # if selected_txs:
-        #     log.info("------before return------")   # e1
+    if not selected_tx:
+        log.info("oh no")
+        return [], "no"
     selected_txs.append(selected_tx)
     total_fee += selected_tx.amount
     remain_txs = []
+    log.info("selected")
     for i in tx_pool1.txs:
         if i.txid != selected_tx.txid and i.amount > 0.1:
             remain_txs.append(i)
     tx_pool1.txs = remain_txs
-            # return selected_txs, total_fee  # e1
     log.info("------before return------")
-    return selected_txs, total_fee  # change 6.20
-
-
+    return selected_txs, total_fee
 
 
 def finding_new_block():
@@ -100,7 +67,11 @@ def finding_new_block():
         log.info("------return these information:" + str(tx3) + str(total_fee) + "------")
         try:
             if tx3:
-                bc1.add_block(tx3, total_fee)    # wait try when there's no transaction
+                bc1.add_block(tx3, total_fee)
+            elif total_fee == "no":
+                log.info("no transaction left")
+                bc1.add_block()
+                log.info("mine a empty block")
         except:
             log.info("------fall behind in mine------")
             try:
@@ -112,7 +83,3 @@ def finding_new_block():
             except:
                 pass
         i += 1
-
-# def start_find():
-#     t1 = threading.Thread(target=finding_new_block,args=()) # change
-#     t1.start()  # change
